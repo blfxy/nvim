adb9e5bd4b27d3e39c2a2739d2a3137cc37213aa
blfxy <s90128696@outlook.com>
2022-10-30 18:58:42 +0800

    保存自动格化

--- lua/lsp/lsp-config.lua
+++ lua/lsp/lsp-config.lua
@@ -14,25 +14,12 @@ require("mason-lspconfig").setup({
 	},
 })
 
--- 指定方式化方式
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
-end
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
 local global_lsp_config = function(custom_config)
 	return {
 		capabilities = require("cmp_nvim_lsp").default_capabilities(),
-		flags = {
-			debounce_text_changes = 150,
-		},
+		-- flags = {
+		-- 	debounce_text_changes = 150,
+		-- },
 		on_attach = function(client, bufnr)
 			-- Enable completion triggered by <c-x><c-o>
 			-- vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
@@ -57,17 +44,6 @@ local global_lsp_config = function(custom_config)
 			-- vim.keymap.set("n", "<space>f", function()
 			-- 	vim.lsp.buf.format({ async = true })
 			-- end, bufopts)
-
-			if client.supports_method("textDocument/formatting") then
-				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = augroup,
-					buffer = bufnr,
-					callback = function()
-						lsp_formatting(bufnr)
-					end,
-				})
-			end
 		end,
 		custom_config,
 	}
--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -1,20 +1,64 @@
-local OK, null_ls = pcall(require, 'null-ls')
+local OK, null_ls = pcall(require, "null-ls")
 if not OK then
 	print("找不到 null-ls")
 	return
 end
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
+
+-- 指定方式化方式
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
+	debug = true,
 	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.rustfmt,
+		formatting.stylua,
+		formatting.rustfmt,
+		formatting.prettier.with({
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+				"css",
+				"html",
+				"json",
+				"less",
+				"scss",
+				"vue",
+			},
+			-- prefer_local = "node_modules/.bin",
+		}),
+
+		-- code_actions.eslint,
+		-- diagnostics.eslint,
+		-- code_actions.gitsigns,
 
 		-- require("null-ls").builtins.diagnostics.eslint,
 		-- require("null-ls").builtins.completion.spell,
 	},
-	on_attach = function(_)
-		-- vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()']])
-		-- if client.resolved_capabilities.document_formatting then
-		--   vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-		-- end
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- vim.lsp.buf.format()
+					lsp_formatting(bufnr)
+				end,
+			})
+		end
 	end,
 })
